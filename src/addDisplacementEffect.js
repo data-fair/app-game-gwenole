0926157816001685112c25ce8bbbcd42e3cd2b55
Schafmann <gwenole.mouton@gmail.com>
2024-12-17 09:14:51 +0100

    init vite and pixi project

---
+++ src/addDisplacementEffect.js
@@ -0,0 +1,14 @@
+import { Sprite, DisplacementFilter } from 'pixi.js';
+
+export function addDisplacementEffect(app) {
+    const sprite = Sprite.from('displacement');
+
+    sprite.texture.baseTexture.wrapMode = 'repeat';
+
+    const filter = new DisplacementFilter({
+        sprite,
+        scale: 50,
+    });
+
+    app.stage.filters = [filter];
+}
